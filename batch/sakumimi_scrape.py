b014df0c129700309c30deb732b762d7e848f00e
oyrarumakan <ryo19911030@hotmail.co.jp>
2026-08-08 12:57:46 +0900

    スクリプトで用いるUAの修正

--- batch/sakumimi_scrape.py
+++ batch/sakumimi_scrape.py
@@ -17,8 +17,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 # 遷移先のURLとUAの設定
 SAKUMIMI_TOP_URL = "https://sakurazaka46.com/s/s46/diary/radio?ima=0000"
 USER_AGENT = (
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"
-    "(KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"
 )
 WAIT_SECONDS = 10
 RETRY_COUNT = 3
